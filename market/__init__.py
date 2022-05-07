e0410e204f474dd5a3af1539fa6a1020f30c84e4
puunina <dokladnie@gmail.com>
2022-05-07 18:33:09 +0200

    update

--- market/__init__.py
+++ market/__init__.py
@@ -2,15 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from market import routes
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' # dictionary by convention key value 'SQLALCHEMY_DATABASE_URI' so flask can identify our database 
 app.config['SECRET_KEY'] = '981651ba9eeb7de5b3227b10'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
-from market import routes
 
 
